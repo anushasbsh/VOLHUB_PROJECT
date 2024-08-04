d4d169d8f03472d218003a542ed49f524e8a3a01
anushasbsh <anusha1sbsh2004@gmail.com>
2024-08-04 12:46:23 +0530

    volhub

--- version1/src/main/java/com/volhub/version1/Model/User.java
+++ version1/src/main/java/com/volhub/version1/Model/User.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.Table; 
 
 @Entity
 @Table(name = "user")
